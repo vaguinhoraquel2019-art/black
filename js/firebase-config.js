c411b6d0c5279b9b776a9e1b2a7532404bfcd50c
vaguinhoraquel2019-art <vaguinhoraquel2019-art@users.noreply.github.com>
2026-09-05 20:11:03 -0300

    feat: sistema de keys com Firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAmwwyNA5yPOdw6-ltK0cmStoNR4viPLE0",
+  authDomain: "anti-governo.firebaseapp.com",
+  projectId: "anti-governo",
+  storageBucket: "anti-governo.appspot.com",
+  appId: "1:955315348495:web:efc1dcf97c2a9d5f90af7a"
+};
+const ADMIN_PASSWORD = "757894nlw";
